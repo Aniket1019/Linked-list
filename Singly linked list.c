54c86a794506e25bed261d1abc500f1c17243e5e
Aniket1019 <125429548+Aniket1019@users.noreply.github.com>
2023-04-26 03:03:29 +0530

    Update Singly linked list.c

--- Singly linked list.c
+++ Singly linked list.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include<stdlib.h>
 
-struct node{
-int data;
-struct node *next;
+struct node
+{
+	int data;
+	struct node *next;
 }; //struct node to hold data and a pointer.
 struct node *start; // starting pointer
 
